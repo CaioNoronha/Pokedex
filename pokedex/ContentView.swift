059e667c6731b5314c59e76feb6f018e5eb88943
Caio Noronha <caio.cnoronha@gmail.com>
2024-04-23 16:05:11 -0300

    creating OnBoarding

---
+++ pokedex/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  pokedex
+//
+//  Created by Caio Noronha on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Caio Noronha on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnBoardingView()
+        }
+    }
+}
